5253d8b389898b47d46624e5820d72864c74f9d3
Florian MARIN <florian@aircall.io>
2018-06-05 13:58:24 +0200

    Direct eliminations : Match papers

--- src/main/java/org/fmarin/admintournoi/admin/match/MatchController.java
+++ src/main/java/org/fmarin/admintournoi/admin/match/MatchController.java
@@ -1,9 +1,9 @@
 package org.fmarin.admintournoi.admin.match;
 
 import com.google.common.collect.Lists;
-import org.fmarin.admintournoi.admin.pool.Pool;
 import org.fmarin.admintournoi.admin.round.Round;
 import org.fmarin.admintournoi.admin.round.RoundRepository;
+import org.fmarin.admintournoi.admin.round.RoundType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,36 +18,43 @@ import java.util.stream.Collectors;
 @RequestMapping("/admin")
 public class MatchController {
 
-    private final MatchRepository matchRepository;
-    private final RoundRepository roundRepository;
+  private final MatchRepository matchRepository;
+  private final RoundRepository roundRepository;
 
-    @Autowired
-    public MatchController(MatchRepository matchRepository, RoundRepository roundRepository) {
-        this.matchRepository = matchRepository;
-        this.roundRepository = roundRepository;
-    }
+  @Autowired
+  public MatchController(MatchRepository matchRepository, RoundRepository roundRepository) {
+    this.matchRepository = matchRepository;
+    this.roundRepository = roundRepository;
+  }
 
-    @PutMapping("/matches/{matchId}")
-    public ResponseEntity update(@PathVariable(name = "matchId") Long matchId, @Valid @ModelAttribute ScorePutView body) {
-        Match match = matchRepository.findOne(matchId);
-        match.setScoreTeam1(body.getTeamScore1());
-        match.setScoreTeam2(body.getTeamScore2());
-        matchRepository.save(match);
-        return ResponseEntity.ok("{\"result\":\"OK\"}");
-    }
+  @PutMapping("/matches/{matchId}")
+  public ResponseEntity update(@PathVariable(name = "matchId") Long matchId, @Valid @ModelAttribute ScorePutView body) {
+    Match match = matchRepository.findOne(matchId);
+    match.setScoreTeam1(body.getTeamScore1());
+    match.setScoreTeam2(body.getTeamScore2());
+    matchRepository.save(match);
+    return ResponseEntity.ok("{\"result\":\"OK\"}");
+  }
 
-    @GetMapping("/rounds/{roundId}/matches/papers")
-    public ModelAndView getPaperMatches(@PathVariable(name = "roundId") Long roundId) {
-        Round round = roundRepository.findOne(roundId);
-        ArrayList<String> elements = Lists.newArrayList(round.getTournament().getFullName(), round.getFullName());
-        String roundName = String.join(" / ", elements);
-        List<MatchPaper> matchPapers = round.getPools().stream()
-                .sorted(Comparator.comparing(Pool::getPosition))
-                .map(pool -> new MatchPaper(roundName, pool.getPosition(), pool.getField(),
-                        pool.getTeam1().getName(), pool.getTeam2().getName(), pool.getTeam3().getName()))
-                .collect(Collectors.toList());
-        return new ModelAndView("match_paper", "papers", matchPapers);
+  @GetMapping("/rounds/{roundId}/matches/papers")
+  public ModelAndView getPaperMatches(@PathVariable(name = "roundId") Long roundId) {
+    Round round = roundRepository.findOne(roundId);
+    ArrayList<String> elements = Lists.newArrayList(round.getTournament().getFullName(), round.getFullName());
+    String roundName = String.join(" / ", elements);
+    if (RoundType.POOL.equals(round.getType())) {
+      List<MatchPaper> matchPapers = round.getPools().stream()
+        .map(pool -> new MatchPaper(roundName, pool.getPosition(), pool.getField(),
+          pool.getTeam1().getName(), pool.getTeam2().getName(), pool.getTeam3().getName()))
+        .collect(Collectors.toList());
+      return new ModelAndView("pool_paper", "papers", matchPapers);
+    } else {
+      List<MatchPaper> matchPapers = round.getPools().stream()
+        .map(pool -> new MatchPaper(roundName, pool.getPosition(), pool.getField(),
+          pool.getTeam1().getName(), pool.getTeam2().getName(), null))
+        .collect(Collectors.toList());
+      return new ModelAndView("match_paper", "papers", matchPapers);
     }
+  }
 
 
 }
