10fb63e312f6a2813df74b38f140ba487643efe9
Florian MARIN <florian@aircall.io>
2018-06-09 23:16:30 +0200

    Pool Papers correctly print

--- src/main/java/org/fmarin/admintournoi/admin/match/MatchController.java
+++ src/main/java/org/fmarin/admintournoi/admin/match/MatchController.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -46,15 +47,29 @@ public class MatchController {
         .map(pool -> new MatchPaper(roundName, pool.getPosition(), pool.getField(),
           pool.getTeam1().getName(), pool.getTeam2().getName(), pool.getTeam3().getName()))
         .collect(Collectors.toList());
-      return new ModelAndView("pool_paper", "papers", matchPapers);
+      return new ModelAndView("pool_paper", "pages", byPage(matchPapers));
     } else {
       List<MatchPaper> matchPapers = round.getPools().stream()
         .map(pool -> new MatchPaper(roundName, pool.getPosition(), pool.getField(),
           pool.getTeam1().getName(), pool.getTeam2().getName(), null))
         .collect(Collectors.toList());
-      return new ModelAndView("match_paper", "papers", matchPapers);
+      return new ModelAndView("match_paper", "pages", byPage(matchPapers));
     }
   }
 
-
+  private List<MatchPage> byPage(List<MatchPaper> papers) {
+    LinkedList<MatchPage> pages = Lists.newLinkedList();
+    for (int i = 0; i < papers.size(); i++) {
+      MatchPage page;
+      if (pages.isEmpty() || i % 2 == 0) {
+        page = new MatchPage();
+        pages.add(page);
+      }
+      else {
+        page = pages.getLast();
+      }
+      page.getPapers().add(papers.get(i));
+    }
+    return pages;
+  }
 }
